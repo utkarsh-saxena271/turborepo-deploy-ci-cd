6a21d32759cbb54e4b2342e7ffaeea7b43a170f7
utkarsh-saxena271 <utkarshsaxena271@gmail.com>
2026-07-10 09:32:44 +0530

    uses prisma in next app

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,7 @@
 import Image, { type ImageProps } from "next/image";
 import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
+import {prisma} from "@repo/db"
 
 type Props = Omit<ImageProps, "src"> & {
   srcLight: string;
